3716ef80bae8493788887251456688f8a642520e
Tim Michals <tcmichals@gmail.com>
2026-09-20 12:04:44 -0500

    net: ethernet: stmmac: dwmac-sun55i: support A733 dedicated syscfg MMIO
    On Allwinner A733 (sun60iw2), GMAC configuration registers (EMAC_CLK / EPIT / ETCS)
    are located in dedicated per-GMAC memory resources (e.g. 0x04508000 for GMAC0)
    rather than the global system control syscon block (0x03000034).
    
    When no 'syscon' property is present, look up the dedicated 'syscfg' (or secondary)
    MMIO resource and program the RGMII interface mode, delay chain, and internal clock gates
    (syscfg + 0x80) via init/exit callbacks. Also set MDC clk_csr = 4, implement fix_soc_reset,
    and use devm_clk_get_optional_enabled for the MBUS clock.
    
    Signed-off-by: Tim Michals <tcmichals@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-sun55i.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-sun55i.c
@@ -11,6 +11,7 @@
 
 #include <linux/bitfield.h>
 #include <linux/bits.h>
+#include <linux/iopoll.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -22,6 +23,7 @@
 
 #include "stmmac.h"
 #include "stmmac_platform.h"
+#include "dwmac4_dma.h"
 
 #define SYSCON_REG		0x34
 
@@ -37,17 +39,70 @@
 #define SYSCON_ETCS_EXT_GMII	0x1
 #define SYSCON_ETCS_INT_GMII	0x2
 
-static int sun55i_gmac200_set_syscon(struct device *dev,
-				     struct plat_stmmacenet_data *plat)
-{
-	struct device_node *node = dev->of_node;
-	struct regmap *regmap;
-	u32 val, reg = 0;
-	int ret;
+/* GMAC210 Clock Gate Register at syscfg + 0x80 */
+#define SUNXI_DWMAC210_CLK_GATE_CFG_REG	0x80
+#define SUNXI_DWMAC210_CLK_GATE_ALL	0xF8
+#define SUNXI_DWMAC210_CFG_ETXDC_H	GENMASK(17, 16)
+#define SUNXI_DWMAC210_CFG_ETXDC_L	GENMASK(12, 10)
 
-	regmap = syscon_regmap_lookup_by_phandle(node, "syscon");
-	if (IS_ERR(regmap))
-		return dev_err_probe(dev, PTR_ERR(regmap), "Unable to map syscon\n");
+struct sun55i_gmac_priv {
+	struct platform_device *pdev;
+	struct regmap *regmap;
+	void __iomem *syscfg_base;
+	u32 syscon_val;
+	bool is_gmac210;
+};
+
+static int sun55i_gmac_init(struct device *dev, void *priv)
+{
+	struct sun55i_gmac_priv *gmac = priv;
+
+	if (gmac->syscfg_base) {
+		writel(gmac->syscon_val, gmac->syscfg_base + 0x00);
+		if (gmac->is_gmac210)
+			writel(0, gmac->syscfg_base + SUNXI_DWMAC210_CLK_GATE_CFG_REG);
+	} else if (gmac->regmap) {
+		regmap_write(gmac->regmap, SYSCON_REG, gmac->syscon_val);
+	}
+
+	return 0;
+}
+
+static void sun55i_gmac_exit(struct device *dev, void *priv)
+{
+	struct sun55i_gmac_priv *gmac = priv;
+
+	if (gmac->syscfg_base) {
+		writel(0, gmac->syscfg_base + 0x00);
+		if (gmac->is_gmac210)
+			writel(SUNXI_DWMAC210_CLK_GATE_ALL, gmac->syscfg_base + SUNXI_DWMAC210_CLK_GATE_CFG_REG);
+	}
+}
+
+static int sun55i_gmac200_set_syscon(struct platform_device *pdev,
+				     struct plat_stmmacenet_data *plat,
+				     struct sun55i_gmac_priv *gmac)
+{
+	struct device *dev = &pdev->dev;
+	struct device_node *node = dev->of_node;
+	struct resource *res;
+	u32 val, reg = 0;
+
+	if (of_property_present(node, "syscon")) {
+		gmac->regmap = syscon_regmap_lookup_by_phandle(node, "syscon");
+		if (IS_ERR(gmac->regmap))
+			return dev_err_probe(dev, PTR_ERR(gmac->regmap), "Unable to map syscon\n");
+	} else {
+		/* A733 / sun60i dedicated per-GMAC syscfg MMIO block */
+		res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "syscfg");
+		if (!res)
+			res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
+		if (!res)
+			return dev_err_probe(dev, -EINVAL, "No syscon or syscfg resource found\n");
+		gmac->syscfg_base = devm_ioremap_resource(dev, res);
+		if (IS_ERR(gmac->syscfg_base))
+			return PTR_ERR(gmac->syscfg_base);
+	}
 
 	if (!of_property_read_u32(node, "tx-internal-delay-ps", &val)) {
 		if (val % 100)
@@ -55,12 +110,21 @@ static int sun55i_gmac200_set_syscon(struct device *dev,
 					     "tx-delay must be a multiple of 100ps\n");
 		val /= 100;
 		dev_dbg(dev, "set tx-delay to %x\n", val);
-		if (!FIELD_FIT(SYSCON_ETXDC_MASK, val))
-			return dev_err_probe(dev, -EINVAL,
-					     "TX clock delay exceeds maximum (%u00ps > %lu00ps)\n",
-					     val, FIELD_MAX(SYSCON_ETXDC_MASK));
+		if (gmac->is_gmac210) {
+			if (val > 31)
+				return dev_err_probe(dev, -EINVAL,
+						     "TX clock delay exceeds maximum (%u00ps > 3100ps)\n",
+						     val);
+			reg |= FIELD_PREP(SUNXI_DWMAC210_CFG_ETXDC_H, val >> 3);
+			reg |= FIELD_PREP(SUNXI_DWMAC210_CFG_ETXDC_L, val & 0x7);
+		} else {
+			if (!FIELD_FIT(SYSCON_ETXDC_MASK, val))
+				return dev_err_probe(dev, -EINVAL,
+						     "TX clock delay exceeds maximum (%u00ps > %lu00ps)\n",
+						     val, FIELD_MAX(SYSCON_ETXDC_MASK));
 
-		reg |= FIELD_PREP(SYSCON_ETXDC_MASK, val);
+			reg |= FIELD_PREP(SYSCON_ETXDC_MASK, val);
+		}
 	}
 
 	if (!of_property_read_u32(node, "rx-internal-delay-ps", &val)) {
@@ -95,17 +159,34 @@ static int sun55i_gmac200_set_syscon(struct device *dev,
 				     phy_modes(plat->phy_interface));
 	}
 
-	ret = regmap_write(regmap, SYSCON_REG, reg);
-	if (ret < 0)
-		return dev_err_probe(dev, ret, "Failed to write to syscon\n");
+	gmac->syscon_val = reg;
+	return sun55i_gmac_init(dev, gmac);
+}
 
-	return 0;
+static int sun55i_gmac_fix_soc_reset(struct stmmac_priv *priv)
+{
+	struct sun55i_gmac_priv *gmac = priv->plat->bsp_priv;
+	void __iomem *ioaddr = priv->ioaddr;
+	u32 value;
+
+	sun55i_gmac_init(priv->device, gmac);
+	usleep_range(1000, 2000);
+
+	/* DMA SW reset */
+	value = readl(ioaddr + DMA_BUS_MODE);
+	value |= DMA_BUS_MODE_SFT_RESET;
+	writel(value, ioaddr + DMA_BUS_MODE);
+
+	return readl_poll_timeout(ioaddr + DMA_BUS_MODE, value,
+				 !(value & DMA_BUS_MODE_SFT_RESET),
+				 10000, 1000000);
 }
 
 static int sun55i_gmac200_probe(struct platform_device *pdev)
 {
 	struct plat_stmmacenet_data *plat_dat;
 	struct stmmac_resources stmmac_res;
+	struct sun55i_gmac_priv *gmac_priv;
 	struct device *dev = &pdev->dev;
 	struct clk *clk;
 	int ret;
@@ -118,15 +199,30 @@ static int sun55i_gmac200_probe(struct platform_device *pdev)
 	if (IS_ERR(plat_dat))
 		return PTR_ERR(plat_dat);
 
+	gmac_priv = devm_kzalloc(dev, sizeof(*gmac_priv), GFP_KERNEL);
+	if (!gmac_priv)
+		return -ENOMEM;
+
+	gmac_priv->pdev = pdev;
+	gmac_priv->is_gmac210 = of_device_is_compatible(dev->of_node, "allwinner,sun60i-a733-gmac210") ||
+				of_device_is_compatible(dev->of_node, "allwinner,sunxi-gmac-210");
+
 	/* BSP disables it */
 	plat_dat->flags |= STMMAC_FLAG_SPH_DISABLE;
+	if (gmac_priv->is_gmac210)
+		plat_dat->flags |= STMMAC_FLAG_MULTI_MSI_EN;
 	plat_dat->host_dma_width = 32;
+	plat_dat->clk_csr = 4; /* MDC clock divider: AHB(200M)/102 = ~2MHz */
+	plat_dat->bsp_priv = gmac_priv;
+	plat_dat->init = sun55i_gmac_init;
+	plat_dat->exit = sun55i_gmac_exit;
+	plat_dat->fix_soc_reset = sun55i_gmac_fix_soc_reset;
 
-	ret = sun55i_gmac200_set_syscon(dev, plat_dat);
+	ret = sun55i_gmac200_set_syscon(pdev, plat_dat, gmac_priv);
 	if (ret)
 		return ret;
 
-	clk = devm_clk_get_enabled(dev, "mbus");
+	clk = devm_clk_get_optional_enabled(dev, "mbus");
 	if (IS_ERR(clk))
 		return dev_err_probe(dev, PTR_ERR(clk),
 				     "Failed to get or enable MBUS clock\n");
@@ -140,6 +236,8 @@ static int sun55i_gmac200_probe(struct platform_device *pdev)
 
 static const struct of_device_id sun55i_gmac200_match[] = {
 	{ .compatible = "allwinner,sun55i-a523-gmac200" },
+	{ .compatible = "allwinner,sun60i-a733-gmac210" },
+	{ .compatible = "allwinner,sunxi-gmac-210" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun55i_gmac200_match);
